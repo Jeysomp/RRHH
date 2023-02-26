b25e9b878f6cbd428e60121d97fbc663411c3ba4
Jeysomp J. Pineda <jj.pineda@gmail.com>
2023-02-26 11:30:14 -0600

    Generación de reportes en pdf en ReportingServices.

---
+++ RRHH/Controllers/ReportController.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Mvc;
+using RRHH.Data;
+
+namespace RRHH.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly RRHHContext _context;
+        public ReportController(RRHHContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult Pdf(string ReportID, string NombreDescarga, ReportArgs args = null)
+        {
+            LocalReport report = new LocalReport("Reports/Empleados.rdl");
+            report.AddDataSource("DSEmpleado", _context.Empleado.ToList());
+            byte[] data = report.Execute(RenderType.Pdf).MainStream;
+            Response.Headers.Add("Content-Disposition", "inline; filename=" + NombreDescarga.Replace(" 	", "_") + ".pdf");
+            return new FileContentResult(data, System.Net.Mime.MediaTypeNames.Application.Pdf);
+        }
+    }
+    public class ReportArgs
+    {
+    }
+}
